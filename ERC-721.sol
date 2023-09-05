98e5c4364e0f7638cbe733daea65134561724281
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2023-09-05 12:17:18 +0200

    Update ERC-721.sol

--- ERC-721.sol
+++ ERC-721.sol
@@ -1,1 +1,23 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GenerativeArt is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("GenerativeArt", "GENART") {}
+
+    function createArt(string memory metadataURI) external returns (uint256) {
+        _tokenIds.increment();
+        uint256 newArtId = _tokenIds.current();
+        
+        _mint(msg.sender, newArtId);
+        _setTokenURI(newArtId, metadataURI);
+
+        return newArtId;
+    }
+}
