a346d5e89e54caeea8eabf1cebb1047e5b6611b5
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2023-09-05 12:18:02 +0200

    Create p5.js

---
+++ p5.js
@@ -0,0 +1,8 @@
+function generateArt() {
+  // Use p5.js or another library to generate art
+  // ...
+
+  // The metadata URI would then be set when calling the smart contract
+  const metadataURI = "ipfs://..."; // replace with actual URI
+  createArt(metadataURI);
+}
